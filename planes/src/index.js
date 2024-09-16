ad93bf09fe3f2fd838c0077b7d94b329de9b5b16
JuanDavidDazaR <150479188+JuanDavidDazaR@users.noreply.github.com>
2024-09-16 11:13:53 -0500

    planes
    Instalar corss en planes

--- planes/src/index.js
+++ planes/src/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const planesController = require('./controllers/planesController');
-const morgan = require('morgan'); 
+const morgan = require('morgan');
+
 const app = express();
+
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.use(planesController);
-
+// Asegúrate de que el controlador esté en la ruta correcta y configurado
+app.use('/planes', planesController);
 
 app.listen(3004, () => {
-  console.log('Microservicio Planes ejecutandose en el puerto 3004');
+  console.log('Microservicio Planes ejecutándose en el puerto 3004');
 });
